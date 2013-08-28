0693671c042102b2df6c29726bf970507a23b798
herrwetzel <herr.wetzel@gmail.com>
2013-08-28 14:28:49 +0100

    changed manage.py so that production settings are automatically chosen when not on local server

--- deptx/manage.py
+++ deptx/manage.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import os
 import sys
+import socket
+
+try:
+    HOSTNAME = socket.gethostname()
+except:
+    HOSTNAME = 'localhost.local'
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deptx.settings")
-
+    if '.local' in HOSTNAME:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deptx.settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deptx.settings_production")
+    
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
