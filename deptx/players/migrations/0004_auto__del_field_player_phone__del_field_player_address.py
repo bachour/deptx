5de8fbdc6117e0b5f6ff68a85eaeb25b18fe6355
herrwetzel <herr.wetzel@gmail.com>
2013-10-02 22:38:39 +0100

    fixed registration

---
+++ deptx/players/migrations/0004_auto__del_field_player_phone__del_field_player_address.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Player.phone'
+        db.delete_column(u'players_player', 'phone')
+
+        # Deleting field 'Player.address'
+        db.delete_column(u'players_player', 'address')
+
+
+    def backwards(self, orm):
+
+        # User chose to not deal with backwards NULL issues for 'Player.phone'
+        raise RuntimeError("Cannot reverse this migration. 'Player.phone' and its values cannot be restored.")
+
+        # User chose to not deal with backwards NULL issues for 'Player.address'
+        raise RuntimeError("Cannot reverse this migration. 'Player.address' and its values cannot be restored.")
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'players.cron': {
+            'Meta': {'object_name': 'Cron'},
+            'activated': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'activationCode': ('django.db.models.fields.CharField', [], {'default': "'d0914321-2baa-11e3-8b43'", 'max_length': '36'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'player': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['players.Player']", 'unique': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        },
+        u'players.mop': {
+            'Meta': {'object_name': 'Mop'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'dob': ('django.db.models.fields.DateField', [], {}),
+            'eyes': ('django.db.models.fields.IntegerField', [], {}),
+            'firstname': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'gender': ('django.db.models.fields.IntegerField', [], {}),
+            'hair': ('django.db.models.fields.IntegerField', [], {}),
+            'height': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lastname': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'marital': ('django.db.models.fields.IntegerField', [], {}),
+            'player': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['players.Player']"}),
+            'serial': ('django.db.models.fields.CharField', [], {'default': "'d0912cde-2baa-11e3-bd9a'", 'max_length': '36'}),
+            'trust': ('django.db.models.fields.IntegerField', [], {'default': '30'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'}),
+            'weight': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'players.player': {
+            'Meta': {'object_name': 'Player'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'firstName': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lastName': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['players']
