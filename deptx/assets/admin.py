a25ae453cd7dfc8d3b3491b882a2ee89ddce0644
herrwetzel <herr.wetzel@gmail.com>
2013-10-09 11:54:44 +0100

    renabled stacked admin inline

--- deptx/assets/admin.py
+++ deptx/assets/admin.py
@@ -1,32 +1,31 @@
 from django.contrib import admin
 from assets.models import Unit, Requisition, Task, Document, Mission, Case
 
-# class CaseDocumentInline(admin.StackedInline):
-#     model = Document
-#     extra = 0
-#     exclude = ['task']
-#     
-# class TaskDocumentInline(admin.StackedInline):
-#     model = Document
-#     extra = 0
-#     exclude = ['case']
-# 
-# 
-# class TaskAdmin(admin.ModelAdmin):
-#     inlines = [TaskDocumentInline]
-# 
-# class CaseAdmin(admin.ModelAdmin):
-#     inlines = [CaseDocumentInline]
+class CaseDocumentInline(admin.TabularInline):
+    model = Document
+    extra = 0
+    exclude = ['task']
+     
+class TaskDocumentInline(admin.StackedInline):
+    model = Document
+    extra = 0
+    exclude = ['case']
+ 
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [TaskDocumentInline]
+ 
+class CaseAdmin(admin.ModelAdmin):
+    inlines = [CaseDocumentInline]
     
 
 
 admin.site.register(Unit)
 admin.site.register(Requisition)
-#admin.site.register(Task, TaskAdmin)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
+#admin.site.register(Task)
 admin.site.register(Document)
 admin.site.register(Mission)
-#admin.site.register(Case, CaseAdmin)
-admin.site.register(Case,)
+admin.site.register(Case, CaseAdmin)
+#admin.site.register(Case,)
 
 
