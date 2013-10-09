7bab031b930237109c9b12e8e532eb89bed4f768
herrwetzel <herr.wetzel@gmail.com>
2013-10-09 01:40:14 +0100

    bugfix

--- deptx/cron/urls.py
+++ deptx/cron/urls.py
@@ -6,7 +6,7 @@ from players import views as players_views
 urlpatterns = patterns('',
     url(r'registration/$', players_views.register, name='players_registration'),
     url(r'registration/([-\w]+)', players_views.activate, name='players_activation'),
-    url(r'changePassword/$', players_views.changePassword, name='players_change_password'),
+    #url(r'changePassword/$', players_views.changePassword, name='players_change_password'),
 
     url(r'^$', views.index, name='cron_index'),
     url(r'login/', views.login, name='cron_login'),
