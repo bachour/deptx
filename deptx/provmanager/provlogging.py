53dce5e5c66f6ebee733b4bb6b33ed94c98b6e51
Trung Dong Huynh <trungdong@donggiang.com>
2013-10-18 00:28:10 +0100

    Provenance logging: Fixed wrong identifiers

--- deptx/provmanager/provlogging.py
+++ deptx/provmanager/provlogging.py
@@ -1,6 +1,6 @@
 from prov.model import ProvBundle, Namespace, Identifier
-from provmanager.models import ProvenanceLog
-from provmanager.views import API
+from .models import ProvenanceLog
+from .views import API
 from deptx.helpers import generateUUID
 
 import datetime
@@ -26,19 +26,22 @@ DEFAULT_NAMESPACES = {
 
 URI = Identifier
 
+
 def getStoreId(cron):
     try:
         provLog = ProvenanceLog.objects.get(cron=cron)
     except ProvenanceLog.DoesNotExist:
         provLog = None
-    
+
     return provLog.store_id
+
 
 def addBundle(cron, bundle, bundle_id):
     store_id = getStoreId(cron)
     #TODO bundle_id is problematic when logging in without having logged out on the mop-page as it does not support auto-logon
-    bundle_id = bundle_id + "/" + str(datetime.datetime.now())
+    bundle_id = bundle_id + "/" + str(time.time())  # Is this really needed?
     API.add_bundle(store_id, bundle, bundle_id)
+
 
 def provlog_add_cron_register(cron):
     bundle_id, bundle = create_bundle_cron_register(cron)
@@ -47,42 +50,52 @@ def provlog_add_cron_register(cron):
     provLog.store_id = store_id
     provLog.save()
 
+
 def provlog_add_cron_login(cron, session_key):
     bundle_id, bundle = create_bundle_cron_login(cron.id, session_key)
     addBundle(cron, bundle, bundle_id)
-    
+
+
 def provlog_add_cron_logout(cron, session_key):
     bundle_id, bundle = create_bundle_cron_logout(cron.id, session_key)
     addBundle(cron, bundle, bundle_id)
+
 
 def provlog_add_mop_register(cron, mop, session_key):
     bundle_id, bundle = create_bundle_mop_register(cron, mop, session_key)
     addBundle(cron, bundle, bundle_id)
 
+
 def provlog_add_mop_login(mop, session_key):
     bundle_id, bundle = create_bundle_mop_login(mop.id, session_key)
     addBundle(mop.player.cron, bundle, bundle_id)
-    
+
+
 def provlog_add_mop_logout(mop, session_key):
     bundle_id, bundle = create_bundle_mop_logout(mop.id, session_key)
     addBundle(mop.player.cron, bundle, bundle_id)
-    
+
+
 def provlog_add_mop_sign_form(mop, requisitionInstance, session_key):
     bundle_id, bundle = create_bundle_mop_sign_form(mop.id, session_key, requisitionInstance.blank.requisition.serial, requisitionInstance.id, requisitionInstance.data)
     addBundle(mop.player.cron, bundle, bundle_id)
-    
+
+
 def provlog_add_mop_send_form(mop, mail, session_key):
     bundle_id, bundle = create_bundle_mop_send_form(mop.id, session_key, mail.requisitionInstance.id, mail.requisitionInstance.blank.requisition.serial, mail.id, mail.unit.serial)
     addBundle(mop.player.cron, bundle, bundle_id)
-    
+
+
 def provlog_add_mop_issue_form(mop, mail, new_requisition, session_key):
     bundle_id, bundle = create_bundle_mop_issue_form(mop.id, session_key, mail.requisitionInstance.id, mail.requisitionInstance.blank.requisition.serial, mail.id, new_requisition.serial, mail.unit.serial)
     addBundle(mop.player.cron, bundle, bundle_id)
 
+
 def provlog_add_mop_issue_document(mop, mail, documentInstance, session_key):
     bundle_id, bundle = create_bundle_mop_issue_document(mop.id, session_key, mail.id, mail.requisitionInstance.blank.requisition.serial, mail.requisitionInstance.id, documentInstance.document.serial, documentInstance.id, mail.unit.serial)
     addBundle(mop.player.cron, bundle, bundle_id)
-    
+
+
 def create_bundle_cron_register(cron):
     '''An cron user account is created'''
     bundle_id = 'b:registration/%d' % cron.id
@@ -95,9 +108,10 @@ def create_bundle_cron_register(cron):
     b.wasAssociatedWith(a, s)
     return bundle_id, b
 
+
 def create_bundle_mop_register(cron, mop, session_key):
     '''An mop user account is created'''
-    bundle_id = 'b:registration/%d' % cron.id
+    bundle_id = 'b:registration/%d' % mop.id
     b = ProvBundle(namespaces=DEFAULT_NAMESPACES)
     s = b.agent('cronuser:%d/%s' % (cron.id, session_key))
     u = b.agent('mopuser:%d' % mop.id, [('foaf:name', mop.user.username)])
@@ -115,11 +129,12 @@ def create_bundle_cron_login(user_id, session_key):
     u = b.agent('cronuser:%d' % user_id)
     ag = b.agent('cronuser:%d/%s' % (user_id, session_key))
     now = datetime.datetime.now()
-    a = b.activity('log:%d/%s' % (user_id, 'login'), now, now, other_attributes=[('prov:type', 'act:CronAccountLogin')])
+    a = b.activity('log:%d/login/%s' % (user_id, session_key), now, now, other_attributes=[('prov:type', 'act:CronAccountLogin')])
     b.wasAssociatedWith(a, u)
     b.wasGeneratedBy(ag, a)
     b.specializationOf(ag, u)
     return bundle_id, b
+
 
 def create_bundle_cron_logout(user_id, session_key):
     bundle_id = 'b:%s/logout' % session_key
@@ -128,9 +143,10 @@ def create_bundle_cron_logout(user_id, session_key):
 
     ag = b.agent('cronuser:%d/%s' % (user_id, session_key))
     now = datetime.datetime.now()
-    a = b.activity('ns:logout', now, now, other_attributes=[('prov:type', 'act:CronAccountLogout')])
+    a = b.activity('log:%d/logout/%s' % (user_id, session_key), now, now, other_attributes=[('prov:type', 'act:CronAccountLogout')])
     b.wasInvalidatedBy(ag, a)  # This user+session no longer exists after this
     return bundle_id, b
+
 
 def create_bundle_mop_login(user_id, session_key):
     '''User id 2 logs into the system'''
@@ -139,11 +155,12 @@ def create_bundle_mop_login(user_id, session_key):
     u = b.agent('mopuser:%d' % user_id)
     ag = b.agent('mopuser:%d/%s' % (user_id, session_key))
     now = datetime.datetime.now()
-    a = b.activity('log:%d/%s' % (user_id, 'login'), now, now, other_attributes=[('prov:type', 'act:MopAccountLogin')])
+    a = b.activity('log:%d/login/%s' % (user_id, session_key), now, now, other_attributes=[('prov:type', 'act:MopAccountLogin')])
     b.wasAssociatedWith(a, u)
     b.wasGeneratedBy(ag, a)
     b.specializationOf(ag, u)
     return bundle_id, b
+
 
 def create_bundle_mop_logout(user_id, session_key):
     bundle_id = 'b:%s/logout' % session_key
@@ -152,9 +169,10 @@ def create_bundle_mop_logout(user_id, session_key):
 
     ag = b.agent('mopuser:%d/%s' % (user_id, session_key))
     now = datetime.datetime.now()
-    a = b.activity('ns:logout', now, now, other_attributes=[('prov:type', 'act:MopAccountLogout')])
+    a = b.activity('log:%d/login/%s' % (user_id, session_key), now, now, other_attributes=[('prov:type', 'act:MopAccountLogout')])
     b.wasInvalidatedBy(ag, a)  # This user+session no longer exists after this
     return bundle_id, b
+
 
 def create_bundle_mop_sign_form(user_id, session_key, blank_form_serial, signed_form_id, form_data=None):
     bundle_id = 'b:%s/form/signing/%d' % (session_key, signed_form_id)
@@ -176,6 +194,7 @@ def create_bundle_mop_sign_form(user_id, session_key, blank_form_serial, signed_
     b.wasAttributedTo(sf, ag)
     return bundle_id, b
 
+
 def create_bundle_mop_send_form(user_id, session_key, signed_form_id, blank_form_serial, email_id, unit_serial):
     bundle_id = 'b:%s/form/sending/%d' % (session_key, email_id)
     b = ProvBundle(namespaces=DEFAULT_NAMESPACES)
@@ -188,6 +207,7 @@ def create_bundle_mop_send_form(user_id, session_key, signed_form_id, blank_form
     b.wasDerivedFrom(em, sf, time=now)
     b.wasAttributedTo(em, ag)
     return bundle_id, b
+
 
 def create_bundle_mop_issue_form(user_id, session_key, signed_form_id, old_blank_form_serial, email_id, new_blank_form_serial, unit_serial):
 #     t = long(time.time())
