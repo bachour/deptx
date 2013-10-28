fa6e545f22458655aac9de51afe4bf13599c3897
herrwetzel <herr.wetzel@gmail.com>
2013-10-28 18:19:09 +0000

    test

--- deptx/mop/admin.py
+++ deptx/mop/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-from mop.models import TaskInstance, Mail, RequisitionInstance, RequisitionBlank, DocumentInstance, Badge #, WeekTrust
+from mop.models import Badge
+#from mop.models import TaskInstance, Mail, RequisitionInstance, RequisitionBlank, DocumentInstance, Badge #, WeekTrust
 from provmanager.models import Provenance
 
-class TaskInstanceAdmin(admin.ModelAdmin):
-    def get_form(self, request, obj=None, **kwargs):
-        form = super(TaskInstanceAdmin,self).get_form(request, obj,**kwargs)
-        form.base_fields['provenance'].queryset = form.base_fields['provenance'].queryset.filter(type=Provenance.TYPE_MOP_INSTANCE)
-        return form
+#class TaskInstanceAdmin(admin.ModelAdmin):
+#    def get_form(self, request, obj=None, **kwargs):
+#        form = super(TaskInstanceAdmin,self).get_form(request, obj,**kwargs)
+#        form.base_fields['provenance'].queryset = form.base_fields['provenance'].queryset.filter(type=Provenance.TYPE_MOP_INSTANCE)
+#        return form
 
-admin.site.register(TaskInstance)
-admin.site.register(Mail)
-admin.site.register(RequisitionInstance)
-admin.site.register(RequisitionBlank)
-admin.site.register(DocumentInstance)
+#admin.site.register(TaskInstance, TaskInstanceAdmin)
+#admin.site.register(Mail)
+#admin.site.register(RequisitionInstance)
+#admin.site.register(RequisitionBlank)
+#admin.site.register(DocumentInstance)
 admin.site.register(Badge)
 #admin.site.register(WeekTrust)
 
