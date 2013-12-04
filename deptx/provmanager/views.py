008ca475b022f3e0e8256060868674436398e19d
herrwetzel <herr.wetzel@gmail.com>
2013-12-04 16:40:48 +0000

    bugfix

--- deptx/provmanager/views.py
+++ deptx/provmanager/views.py
@@ -67,10 +67,10 @@ def view_randomize(request, id):
     return render(request, 'provmanager/view_randomize.html', {'provenance_name':provenance.name, 'provenance_store_id':provenance.store_id, 'json_str':json.dumps(clean_graph) })
 
 
-def getStuff(request, folder):
+def getStuff(request):
     filename = request.POST["filename"]
     request.session['filename'] = filename
-    full = "http://univeam1.miniserver.com/media/PROVENANCE/test/%s" % (folder, filename)
+    full = "http://univeam1.miniserver.com/media/PROVENANCE/test/%s" % (filename)
     response = urllib2.urlopen(full)
     content = response.read()
     response.close()
