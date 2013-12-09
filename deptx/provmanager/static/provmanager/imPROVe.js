9e11e305fa3794a3997b3119cf42361cc284bfd5
bachour <khaled.bachour@nottingham.ac.uk>
2013-12-09 13:14:53 +0000

    Made media inspector image fixed Width, instead of fixed height

--- deptx/provmanager/static/provmanager/imPROVe.js
+++ deptx/provmanager/static/provmanager/imPROVe.js
@@ -1847,7 +1847,7 @@ function createAndAddMediaJQueryDialog(url, id)
 	else // if image
 	{
 		url = URL_MEDIA + url;
-		innerHTML = '<img style="display:block;margin:0 auto 0 auto" src="' + url + '" height="' + SCREEN_HEIGHT*0.8 +'" id="image">';
+		innerHTML = '<img style="display:block;margin:0 auto 0 auto" src="' + url + '" width="' + SCREEN_WIDTH*0.5 +'" id="image">';
 	}
 	var dialog = document.getElementById(dialogID);
 	dialog.innerHTML = innerHTML;
