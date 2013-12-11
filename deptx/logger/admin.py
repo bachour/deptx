8824bb04fd10ba7c268b1c5f29dbec92e48adc98
herrwetzel <herr.wetzel@gmail.com>
2013-12-11 13:45:14 +0000

    increasing speed of action logs

--- deptx/logger/admin.py
+++ deptx/logger/admin.py
@@ -3,6 +3,8 @@ from logger.models import ProvLog, ActionLog
 
 class ActionLogAdmin(admin.ModelAdmin):
     list_filter = ('action', 'cron', 'mop', 'createdAt')
+    raw_id_fields = ('cron', 'mission', 'case', 'questionInstance', 'cronDocument', 'cronDocumentInstance', 'message', 'mop', 'mail', 'mopDocumentInstance', 'requisitionInstance')
+
 
 class ProvLogAdmin(admin.ModelAdmin):
     list_filter = ('action', 'cronDocumentInstance__cron', 'mopDocumentInstance__mop', 'createdAt')
