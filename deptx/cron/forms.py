8f2e9857a5e68429988c16f1de9d73018f6a33fd
herrwetzel <herr.wetzel@gmail.com>
2014-02-06 11:39:10 +0000

    added chat added jean baker message

--- deptx/cron/forms.py
+++ deptx/cron/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 
-from models import HelpMail#, ChatMessage
+from models import HelpMail, ChatMessage
 from players.models import Cron
 
 class HelpMailForm(ModelForm):
@@ -15,8 +15,8 @@ class ControlHelpMailForm(ModelForm):
         model = HelpMail
         fields = ['cron', 'body']
 
-# class ChatForm(ModelForm):
-#     class Meta:
-#         model = ChatMessage
-#         fields = ['message' ]
+class ChatForm(ModelForm):
+    class Meta:
+        model = ChatMessage
+        fields = ['message' ]
     
