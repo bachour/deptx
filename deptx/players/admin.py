2d650dae804136c7b3efe6d66853de9315e95ff1
herrwetzel <herr.wetzel@gmail.com>
2014-03-25 12:25:50 +0000

    filtering by email of cron players

--- deptx/players/admin.py
+++ deptx/players/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from players.models import Player, Cron, Mop
 
+class CronAdmin(admin.ModelAdmin):
+    list_filter = ('email',)
+
 admin.site.register(Player)
-admin.site.register(Cron)
+admin.site.register(Cron, CronAdmin)
 admin.site.register(Mop)
